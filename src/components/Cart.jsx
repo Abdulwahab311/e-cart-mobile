6ce08dbd158c882d101ff1c4a9f9b03a0d901ed7
Abdulwahab311 <157164848+Abdulwahab311@users.noreply.github.com>
2024-03-18 00:10:23 +0500

    My E-Cart Site

---
+++ src/components/Cart.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Cart = (cart, setCart) => {
+  return (
+    <>
+    <div className="carding">
+    <q>This item is Successfully Added in the cart thnks alot for the Shopping!!</q>
+    </div>
+    </>
+  )
+}
+
+export default Cart;
---
+++ src/components/SearchItem.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { items } from './Data';
+import Product from './Product';
+
+const SearchItem = () => {
+  const{term} = useParams();
+  const [filterData,setFilterData] =useState([]);
+ useEffect(()=>{
+  const filterData = ()=>{
+    const data = items.filter((p)=>p.title.toLowerCase().includes(term.toLowerCase()));
+    setFilterData(data);
+  }
+  filterData();
+ }, [term])
+
+  return (
+    <Product items={filterData}/>
+  )
+}
+
+export default SearchItem
